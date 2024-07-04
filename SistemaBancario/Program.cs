22bce6a9451471399543e304905937c9e792b1eb
Pietra Carneiro <pietrasc98@gmail.com>
2024-07-03 23:00:31 -0300

    initial commit

---
+++ SistemaBancario/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaBancario
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContaBancaria cc = new ContaBancaria("12345", 500);
+            cc.Depositar(1000);
+        }
+    }
+}
